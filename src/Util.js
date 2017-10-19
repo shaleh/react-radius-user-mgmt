02eb76592a845cddda2dc0c41d4eb9c4668f8bdd
Sean 'Shaleh' Perry <shaleh@speakeasy.net>
2017-10-18 17:23:36 -0700

    Enable flow.

--- src/Util.js
+++ src/Util.js
@@ -1,13 +1,13 @@
-function checkStatus(response) {
+// @flow
+function checkStatus(response: Response) {
     if (response.status >= 200 && response.status < 300) {
         return response;
     }
     const error = new Error(`HTTP Error ${response.statusText}`);
-    console.log(error); // eslint-disable-line no-console
     throw error;
 }
 
-function parseJSON(response) {
+function parseJSON(response: Response) {
     return response.json();
 }
 
